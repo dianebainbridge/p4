4090ba53d2a8eb53fc67a79513f8c84ca825a884
dianebainbridge <diane.bainbridge@gmail.com>
2019-05-15 20:10:33 -0400

    Uploading final changes

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -62,6 +62,13 @@
 
 <footer>
     <a href='{{ config('app.githubUrl') }}'><i class='fab fa-github'></i> View p4 on Github</a>
+    <p style="text-align:right;margin-top:-35px;padding-right:10px">
+        <a href="http://jigsaw.w3.org/css-validator/check/referer">
+            <img style="border:0;width:88px;height:31px"
+                 src="http://jigsaw.w3.org/css-validator/images/vcss-blue"
+                 alt="Valid CSS!" />
+        </a>
+    </p>
 </footer>
 
 </body>
